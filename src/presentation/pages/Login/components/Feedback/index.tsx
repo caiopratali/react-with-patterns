e342bce0aeeee3cb42b16baddd8d9dadcabf3a45
Caio Pratali <caio@pratali.com.br>
2022-04-16 22:20:22 -0300

    feat: ensure login will not load spinner and error on start

--- src/presentation/pages/Login/components/Feedback/index.tsx
+++ src/presentation/pages/Login/components/Feedback/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Spinner } from '@/presentation/components';
+import Context from '@/presentation/contexts/form/form';
 
 import styles from './styles.scss';
 
 const Feedback: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context);
   return (
-    <div className={styles.errorWrap}>
-      <Spinner className={styles.spinner} />
-      <span>Erro</span>
+    <div data-testid="error-wrap" className={styles.errorWrap}>
+      {isLoading && <Spinner className={styles.spinner} />}
+      {errorMessage && <span>{errorMessage}</span>}
     </div>
   );
 };
--- src/presentation/pages/Login/components/Form/index.tsx
+++ src/presentation/pages/Login/components/Form/index.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Input } from '@/presentation/components/Form';
+import Context from '@/presentation/contexts/form/form';
 import { Feedback } from '../Feedback';
 
 import './styles.scss';
 
+type StateProps = {
+  isLoading: boolean;
+  errorMessage: string;
+};
+
 const Form: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  });
+
   return (
-    <form action="">
-      <h2>Login</h2>
-      <Input type="email" name="email" placeholder="Digite o seu e-mail" />
-      <Input type="password" name="password" placeholder="Digite o sua senha" />
-      <button type="submit">Entrar</button>
-      <span>Criar conta</span>
-      <Feedback />
-    </form>
+    <Context.Provider value={state}>
+      <form action="">
+        <h2>Login</h2>
+        <Input type="email" name="email" placeholder="Digite o seu e-mail" />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Digite o sua senha"
+        />
+        <button type="submit">Entrar</button>
+        <span>Criar conta</span>
+        <Feedback />
+      </form>
+    </Context.Provider>
   );
 };
 
